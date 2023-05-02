f1cbf032a10b58270dc59975001f47f5d1aa46c7
Carl Alaeddin <carl.alaeddin@gmail.com>
2023-05-02 15:41:06 +0330

    added edit and show route

--- routes/FrontendRouteGroups/FrontendRoute.php
+++ routes/FrontendRouteGroups/FrontendRoute.php
@@ -7,4 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/')->controller(FrontendController::class)->group(function (){
    Route::get('/','index')->name('index');
+   Route::get('/edit/{album}','edit')->name('album.edit');
+   Route::get('/show/{show}','show')->name('album.show');
 });
