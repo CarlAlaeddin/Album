2f8cffa52023f617f2bfc0867c62fa989be2f382
Carl Alaeddin <carl.alaeddin@gmail.com>
2023-05-02 15:05:36 +0330

    added frontend route group

--- routes/FrontendRouteGroups/FrontendRoute.php
+++ routes/FrontendRouteGroups/FrontendRoute.php
@@ -1,1 +1,10 @@
 <?php
+
+use App\Http\Controllers\Frontend\FrontendController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::prefix('/')->controller(FrontendController::class)->group(function (){
+   Route::get('/','index')->name('index');
+});
