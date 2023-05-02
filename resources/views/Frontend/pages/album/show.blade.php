a9ce97f715c7e57f4a47249bd3091fae46c833d0
Carl Alaeddin <carl.alaeddin@gmail.com>
2023-05-02 15:59:29 +0330

    added details to page

--- resources/views/Frontend/pages/album/show.blade.php
+++ resources/views/Frontend/pages/album/show.blade.php
@@ -1,4 +1,12 @@
 @extends('Frontend.layouts.app')
 @section('content')
-
+    <div class="card">
+        <h5 class="card-header">ID Photo {{ $album->id }}</h5>
+        <div class="card-header">
+            <img src="{{ $album->image }}" alt="">
+        </div>
+        <div class="card-body">
+            {{ $album->description }}
+        </div>
+    </div>
 @endsection
