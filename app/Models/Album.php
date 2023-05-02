a0de132ee71443fc39335939ad994d0ae1213761
Carl Alaeddin <carl.alaeddin@gmail.com>
2023-05-02 15:30:16 +0330

    added relations one to many album-user

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Album extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'image',
+        'description',
+        'is_status',
+    ];
+
+
+    /**
+     * @return HasMany
+     */
+    public function albums(): HasMany
+    {
+        return $this->hasMany(Album::class);
+    }
+
 }
