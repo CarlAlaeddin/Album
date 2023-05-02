c11c5dd47d9f1af202ec77d83857ae3a30a76ee2
Carl Alaeddin <carl.alaeddin@gmail.com>
2023-05-02 16:03:24 +0330

    debug id for show method

--- routes/FrontendRouteGroups/FrontendRoute.php
+++ routes/FrontendRouteGroups/FrontendRoute.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AlbumController;
 use App\Http\Controllers\Frontend\FrontendController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,6 +8,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/')->controller(FrontendController::class)->group(function (){
    Route::get('/','index')->name('index');
-   Route::get('/edit/{album}','edit')->name('album.edit');
-   Route::get('/show/{show}','show')->name('album.show');
+
+});
+
+
+Route::prefix('/album')->controller(AlbumController::class)->name('album.')->group(function (){
+    Route::get('/edit/{album}','edit')->name('edit');
+    Route::get('/show/{album}','show')->name('show');
 });
