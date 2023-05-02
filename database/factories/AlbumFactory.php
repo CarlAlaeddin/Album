f5fabd898f00a963291c0b435b4d80c97ec6b9d7
Carl Alaeddin <carl.alaeddin@gmail.com>
2023-05-02 15:27:23 +0330

    fixed Album tools

---
+++ database/factories/AlbumFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use JetBrains\PhpStorm\ArrayShape;
+
+/**
+ * @extends Factory
+ */
+class AlbumFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    #[ArrayShape([
+            'user_id'       =>          "\Illuminate\Support\HigherOrderCollectionProxy|mixed",
+            'image'         =>          "string",
+            'description'   =>          "string",
+            'is_status'     =>          "int"
+        ]
+    )]
+    public function definition(): array
+    {
+        return [
+            'user_id'       =>          User::all()->random()->id,
+            'image'         =>          $this->faker->imageUrl,
+            'description'   =>          $this->faker->realText,
+            'is_status'     =>          $this->faker->numberBetween(0, 1),
+        ];
+    }
+}
