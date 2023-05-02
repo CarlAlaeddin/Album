dddd5e018ca4da5e8b2b5de3b28a2e4d986e0f2a
Carl Alaeddin <carl.alaeddin@gmail.com>
2023-05-02 15:40:36 +0330

    added foreach and get albums model for loop

--- resources/views/Frontend/index.blade.php
+++ resources/views/Frontend/index.blade.php
@@ -1,21 +1,20 @@
 @extends('Frontend.layouts.app')
 @section('content')
+    @foreach($albums as $album)
     <div class="col">
-        <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c"/>
-                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-            </svg>
+        <div class="card shadow-sm rounded">
+            <img class="img-fluid" src="{{ $album->image }}" alt="{{ \Illuminate\Support\Str::limit($album->description,20) }}">
             <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text">{{ \Illuminate\Support\Str::limit($album->description) }}</p>
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                        <a href="{{ route('album.show',$album->id) }}" class="btn btn-sm btn-outline-secondary">View</a>
+                        <a href="{{ route('album.edit',$album->id) }}" class="btn btn-sm btn-outline-secondary">Edit</a>
                     </div>
-                    <small class="text-body-secondary">9 mins</small>
+                    <small class="text-body-secondary">{{ $album->created_at->format('Y-M-d | H:m:s') }}</small>
                 </div>
             </div>
         </div>
     </div>
+    @endforeach
 @endsection
