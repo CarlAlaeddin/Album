8ed276228fcedfba5d4b0cbaf579bff85c783587
Carl Alaeddin <carl.alaeddin@gmail.com>
2023-05-02 15:26:54 +0330

    added album factory and edit user create factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Album;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        #_____________________________________ User Factory
+         User::factory(9)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         User::factory()->create([
+             'name' => 'Carl Alaeddin',
+             'email' => 'carl.alaeddin@gmail.com',
+         ]);
+
+        #_____________________________________ Album Factory
+        Album::factory(100)->create();
     }
 }
