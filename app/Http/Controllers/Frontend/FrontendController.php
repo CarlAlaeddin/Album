190099386eccfe4e9f41b6c9320cedfb66dabb10
Carl Alaeddin <carl.alaeddin@gmail.com>
2023-05-02 15:04:54 +0330

    added frontend controller

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
-    //
+    /**
+     * @return Factory|Application|View|\Illuminate\Contracts\Foundation\Application
+     */
+    public function index(): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
+    {
+        return view('Frontend.index');
+    }
 }
